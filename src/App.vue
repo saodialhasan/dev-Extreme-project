403ae0a6ef3d5263053b10c9db5cee00032d77a2
[saodial hasan] <saodialhasan@gmail.com>
2025-05-06 17:29:17 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import DxButton from 'devextreme-vue/button';
+ function showMessage(){
+  alert("HEllo Developers")
+ }
+import SelectItem from './components/SelectItem.vue';
+</script>
+
+<template>
+  <header class="max-w-300 m-auto">
+    <div>Dev extrem</div>
+    <DxButton text="Click me!" @click="showMessage()" />
+<SelectItem/>
+  </header>
+  <main>
+
+  </main>
+</template>
+
+<style scoped></style>
